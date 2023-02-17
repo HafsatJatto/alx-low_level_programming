404776523c2f808c0e36c6242b62796921b2467e
hafsat jatto <jattohafsat@gmail.com>
2023-02-17 13:11:14 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char hj;
+
+	for (hj = 'a'; hj <= 'z'; hj++)
+	{
+		putchar(hj);
+	}
+	putchar('\n');
+	return (0);
+}
