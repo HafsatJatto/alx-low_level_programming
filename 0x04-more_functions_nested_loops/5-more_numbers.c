ac08f7ea356565dfa27f8f0844e971fb927b37a0
hafsat jatto <jattohafsat@gmail.com>
2023-02-23 17:40:18 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,16 +3,14 @@
 
 /**
  * more_numbers - Print 10 times the numbers from 0 up to 14
- * Return: 10 times of the numbers from 0 up to 14
  */
 
 void more_numbers(void)
-
 {
 
 	int x, y;
 
-	for (x = 0; x < 10; x++)
+	for (x = 1; x <= 10; x++)
 	{
 	for (y = 0; y <= 14; y++)
 	{
@@ -20,7 +18,7 @@ void more_numbers(void)
 	{
 	_putchar((y / 10) + '0');
 	}
-	_putchar((y % 10) + "0");
+	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
 	}
